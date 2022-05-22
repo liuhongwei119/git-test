b7d5ae99c1c0e1fa4941898fd5fb17b767583419
liuhongwei119 <769591656@qq.com>
2022-05-22 20:35:06 +0800

    third commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -1,9 +1,10 @@
 package com.atguigu;
 
-public class gitTest {
+public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git222");
+        System.out.println("hello git333");
     }
 }
